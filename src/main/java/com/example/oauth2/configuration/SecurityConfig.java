29ed3dd2a7207652cfda934f9c4d3c1f8569c5f5
cheoljin408 <kcj409@naver.com>
2022-07-19 10:54:24 +0900

    oauth server 수정

--- src/main/java/com/example/oauth2/configuration/SecurityConfig.java
+++ src/main/java/com/example/oauth2/configuration/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.oauth2.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,5 +17,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin() // 기본 제공하는 로그인 화면 사용
                 .and()
                 .httpBasic(); // http 통신으로 basic auth를 사용 할 수 있다. (ex: Authorization: Basic bzFbdGfmZrptWY30YQ==)
+    }
+
+    /**
+     * authenticationManager bean 생성 하여 셋팅 안할시 grant_type : password 지원 안함
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
