fa78557c9d64a6a017ad02ac3a31bdf6547da705
cheoljin408 <kcj409@naver.com>
2022-07-19 16:17:39 +0900

    simple oauth2

--- src/main/java/com/example/oauth2/Oauth2Application.java
+++ src/main/java/com/example/oauth2/Oauth2Application.java
@@ -2,8 +2,12 @@ package com.example.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
 public class Oauth2Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/oauth2/configuration/Oauth2AuthorizationConfig.java
+++ src/main/java/com/example/oauth2/configuration/Oauth2AuthorizationConfig.java
@@ -1,62 +1,35 @@
 package com.example.oauth2.configuration;
 
-import com.example.oauth2.service.UserDetailService;
-import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.patterns.ITokenSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-import javax.sql.DataSource;
-
-@EnableAuthorizationServer
 @Configuration
-@RequiredArgsConstructor
 public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
-    private final AuthenticationManager authenticationManager;
-    private final DataSource dataSource;
-    private final UserDetailService userDetailService;
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()")
-                .allowFormAuthenticationForClients();
-    }
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     // client 설정
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory() // 클라이언트 정보는 메모리를 이용 한다.
-                .withClient("clientId") // 클라이언트 아이디
-                .secret("{noop}secretKey") // 시크릿키 ({} 안에 암호화 알고리즘을 명시 하면 된다. 암호화가 되어 있지 않다면 {noop}로 설정 해야 한다. 실제 요청은 암호화 방식인 {noop}를 입력 하지 않아도 된다.)
-                .authorizedGrantTypes("authorization_code","password", "refresh_token") // 가능한 토큰 발행 타입
-                .scopes("read", "write") // 가능한 접근 범위
-                .accessTokenValiditySeconds(60) // 토큰 유효 시간 : 1분
-                .refreshTokenValiditySeconds(60*60) // 토큰 유효 시간 : 1시간
-                .redirectUris("http://localhost:8081/callback") // 가능한 redirect uri
-                .autoApprove(true); // 권한 동의는 자동으로 yes (false 로 할시 권한 동의 여부를 묻는다.)
+        clients.inMemory()
+                .withClient("service1")
+                .secret("{noop}service1secret")
+                .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+                .scopes("webclient", "mobileclient");
     }
 
     // 인증, 토큰 설정
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userDetailService) // refrash token 발행시 유저 정보 검사 하는데 사용하는 서비스 설정
-                .tokenStore(tokenStore());
+                .userDetailsService(userDetailsService);
     }
 }
